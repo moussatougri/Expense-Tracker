d6877b9dd39722bb05bb84e93aaa26a8d43337a6
moussatougri <moussatougri23@gmail.com>
2022-07-15 22:26:31 +0200

    submit data

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log();
+//Selektoren
+const inputName = document.querySelector(".name");
+const inputDate = document.querySelector(".date");
+const inputAmount = document.querySelector(".amount");
+const formularInp = document.querySelectorAll("#formular input");
+const formular = document.querySelector(".formular");
+
+const submitbtn = document.querySelector(".submit-btn");
+
+//state Form
+const state = {
+  item: [],
+};
+
+function submitForm(e) {
+  e.preventDefault();
+  const array = Array.from(formularInp).reduce(
+    (acc, input) => ({ ...acc, [input.id]: input.value }),
+    {}
+  );
+
+  console.log(array);
+  state.item.push(array);
+  console.log(state.item);
+}
+
+formular.addEventListener("submit", submitForm);
