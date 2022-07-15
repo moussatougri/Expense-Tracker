9961c782f0a1da7fbdf2a9d4ae1894d39cfd3e27
moussatougri <moussatougri23@gmail.com>
2022-07-15 22:56:31 +0200

    render item

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const inputDate = document.querySelector(".date");
 const inputAmount = document.querySelector(".amount");
 const formularInp = document.querySelectorAll("#formular input");
 const formular = document.querySelector(".formular");
+const table = document.querySelector(".table");
 
 const submitbtn = document.querySelector(".submit-btn");
 
@@ -14,14 +15,28 @@ const state = {
 
 function submitForm(e) {
   e.preventDefault();
+  //get data
   const array = Array.from(formularInp).reduce(
     (acc, input) => ({ ...acc, [input.id]: input.value }),
     {}
   );
 
-  console.log(array);
   state.item.push(array);
-  console.log(state.item);
+
+  //render Item
+  let tdElementName = document.createElement("td");
+  let tdElementDate = document.createElement("td");
+  let tdElementAmount = document.createElement("td");
+  const trElement = document.createElement("tr");
+  tdElementName.innerText = array.name;
+  tdElementDate.innerText = array.date;
+  tdElementAmount.innerText = array.amount;
+  //appendchild
+  table.appendChild(trElement);
+  trElement.appendChild(tdElementName);
+  trElement.appendChild(tdElementDate);
+  trElement.appendChild(tdElementAmount);
+  console.log(table);
 }
 
 formular.addEventListener("submit", submitForm);
