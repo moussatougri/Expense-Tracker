2ef1c4a742538736f2e5916e09745b6ac94aa28e
moussatougri <moussatougri23@gmail.com>
2022-07-18 18:52:36 +0200

    add localstorage

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 //Selektoren
-const inputName = document.querySelector(".name");
-const inputDate = document.querySelector(".date");
-const inputAmount = document.querySelector(".amount");
 const formularInp = document.querySelectorAll("#formular input");
 const formular = document.querySelector(".formular");
 const table = document.querySelector(".table");
@@ -10,7 +7,7 @@ const submitbtn = document.querySelector(".submit-btn");
 
 //state Form
 const state = {
-  item: [],
+  items: [],
 };
 
 function submitForm(e) {
@@ -21,9 +18,13 @@ function submitForm(e) {
     {}
   );
 
-  state.item.push(array);
+  state.items.push(array);
+
+  saveLocalTodos(array);
 
   //render Item
+
+  //create Item
   let tdElementName = document.createElement("td");
   let tdElementDate = document.createElement("td");
   let tdElementAmount = document.createElement("td");
@@ -31,12 +32,88 @@ function submitForm(e) {
   tdElementName.innerText = array.name;
   tdElementDate.innerText = array.date;
   tdElementAmount.innerText = array.amount;
+
+  //delete Item
+  const trForDeleteSection = document.createElement("tr");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("trash-btn");
+  deleteBtn.addEventListener("click", deleteItemfromArray);
+
   //appendchild
   table.appendChild(trElement);
+  table.appendChild(trForDeleteSection);
   trElement.appendChild(tdElementName);
   trElement.appendChild(tdElementDate);
   trElement.appendChild(tdElementAmount);
-  console.log(table);
+  trElement.appendChild(deleteBtn);
 }
 
-formular.addEventListener("submit", submitForm);
+function checkLocalItem() {
+  if (localStorage.getItem("item") === null) {
+    state.items;
+  } else {
+    state.items = JSON.parse(localStorage.getItem("item"));
+  }
+}
+
+//save todo in localstorage
+function saveLocalTodos(item) {
+  checkLocalItem();
+  state.items.push(item);
+  localStorage.setItem("item", JSON.stringify(state.items));
+}
+
+function getItems() {
+  checkLocalItem();
+  state.items.forEach(function (item) {
+    //render Item
+    let tdElementName = document.createElement("td");
+    let tdElementDate = document.createElement("td");
+    let tdElementAmount = document.createElement("td");
+    const trElement = document.createElement("tr");
+    tdElementName.innerText = item.name;
+    tdElementDate.innerText = item.date;
+    tdElementAmount.innerText = item.amount;
+
+    //delete Item
+    const trForDeleteSection = document.createElement("tr");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("trash-btn");
+
+    //appendchild
+    table.appendChild(trElement);
+    table.appendChild(trForDeleteSection);
+    trElement.appendChild(tdElementName);
+    trElement.appendChild(tdElementDate);
+    trElement.appendChild(tdElementAmount);
+    trElement.appendChild(deleteBtn);
+  });
+}
+
+//delete Item
+function deleteItemfromArray(e) {
+  const item = e.target;
+  if (item.classList[0] === "trash-btn") {
+    //delete item from the window
+    const deleteItem = items.parentElement;
+    deleteItem.remove();
+    //delete item from an array
+    const index = state.items.findIndex(
+      (x) => x.description === deleteItem.innerText
+    );
+    state.items.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(state.items));
+  }
+}
+
+function eventListener() {
+  formular.addEventListener("submit", submitForm);
+  document.addEventListener("DOMContentLoaded", getItems);
+  formular.addEventListener("keyup", function (e) {
+    if (e.key === "Enter") {
+      submitForm(e);
+    }
+  });
+}
+
+eventListener();
